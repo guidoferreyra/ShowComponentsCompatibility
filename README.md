121f40cbe719750366e04d5a8a871d5e6d9ec236
Guido Ferreyra <guido@tipografia.com.ar>
2018-09-22 13:45:59 -0300

    First commit

---
+++ README.md
@@ -0,0 +1,25 @@
+Show Components Compatibility
+==============
+This plugin is a complementary tool for Show Masters Compatibility for those cases where the masters are not compatible because the abscense or different components.
+
+This plugin shows in a light orange colour the list of anchors used in the glyph of the next font and in gray colour the list of current font anchors.
+If anchors names and quantity matches between fonts it display the word "OK" in green.
+
+The plugin displays the names of the components in the current layer if the component is missing in other master it uses red color on the component name label.
+
+### How to use:
+Activate the plugin via **View>Show Components Compatibility** menu item.
+
+### Installation
+Download the plugin, open it and restart glyphs.
+
+
+### Donate
+If this plugin is helpful for you, maybe you can consider making a donation ;)
+
+[![Donate](https://img.shields.io/badge/Donate-PayPal-green.svg)](https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=NXQFEWCXXJABE&lc=US&item_name=Github%20Donate&currency_code=USD&bn=PP%2dDonationsBF%3abtn_donate_LG%2egif%3aNonHosted)
+
+![](screen-anchorscompat.png)
+
+
+
---
+++ ShowComponentsCompatibility.glyphsReporter/Contents/Resources/plugin.py
@@ -0,0 +1,62 @@
+# encoding: utf-8
+
+import objc
+import sys, os, re
+import math
+
+from GlyphsApp import *
+from GlyphsApp.plugins import *
+
+class ShowComponentsCompatibility (ReporterPlugin):
+	def settings(self):
+		self.menuName = "Components Compatibility"
+	
+	def checkComponents (self, Layer):
+
+		thisFont = Glyphs.font
+		currentLayer = thisFont.selectedLayers[0]
+		thisGlyph = currentLayer.parent
+
+		HandleSize = self.getHandleSize()
+		scale = self.getScale()
+		zoomedHandleSize = HandleSize / scale
+
+		initPos = -30
+		step = 24	
+		xHeight = thisFont.selectedFontMaster.xHeight
+		angle = thisFont.selectedFontMaster.italicAngle
+		offset = math.tan(math.radians(angle)) * xHeight/2
+
+		lista = []
+
+		for thisMaster in thisFont.masters: 
+			thisMasterId = thisMaster.id
+			masterIndex = thisFont.masters.index(thisMaster)
+			frontLayers = thisGlyph.layers[thisFont.masters[masterIndex].id]
+			
+			for thisComponent in frontLayers.components:
+				lista.append(str(thisComponent.name))
+			
+		fontColor1 = NSColor.colorWithCalibratedRed_green_blue_alpha_(1, 0, 0, 0.7)
+		fontColor2 = NSColor.colorWithCalibratedRed_green_blue_alpha_( 0, 0, 0, 0.45 )
+
+		for currentComponent in currentLayer.components:
+		
+			name = currentComponent.name
+			posX = currentComponent.position.x
+			posY = currentComponent.position.y
+			
+			if lista.count(currentComponent.name) != len(thisFont.masters):
+				self.drawTextAtPoint(u"%s" % name, (10 - offset, initPos), 14.0, fontColor1)
+			else:
+				self.drawTextAtPoint(u"%s" % name, (10 - offset, initPos), 14.0, fontColor2)
+
+			initPos = initPos - step
+
+	def background(self, Layer):
+		try:
+			NSColor.colorWithCalibratedRed_green_blue_alpha_(0.0, 0.5, 0.3, 0.5).set()
+			self.checkComponents(Layer)
+		except Exception as e:
+			import traceback
+			print traceback.format_exc()
